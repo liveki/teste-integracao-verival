e24269b4cecb0739a545a5c737ef973c0f272a02
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-05-27 17:44:01 -0300

    initial commit

---
+++ src/main/java/com/trabalhoverival/app/App.java
@@ -0,0 +1,13 @@
+package com.trabalhoverival.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/trabalhoverival/app/Numero.java
@@ -0,0 +1,36 @@
+package com.trabalhoverival.app;
+
+public class Numero {
+    private Integer valor;
+
+    public Numero(int valor) {
+        this.valor = valor;
+    }
+
+    public int valor() {
+        return valor;
+    }
+
+    public Numero inc() {
+        valor++;
+        return (this);
+    }
+
+    public Numero dec() {
+        valor--;
+        return this;
+    }
+
+    public Numero abs() {
+        valor = Math.abs(valor);
+        return this;
+    }
+
+    public Numero duplica() {
+        return new Numero(valor);
+    }
+
+    public String toString() {
+        return valor.toString();
+    }
+}
