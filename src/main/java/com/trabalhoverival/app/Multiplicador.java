375dfe0a1844e8a16053dfbfcc4debf86aa994bb
LuizGuerra <luizpedrofg@gmail.com>
2020-05-27 19:24:17 -0300

    beggining

---
+++ src/main/java/com/trabalhoverival/app/Multiplicador.java
@@ -0,0 +1,26 @@
+// package com.trabalhoverival.app;
+
+// public class Multiplicador {
+//     private Somador somador;
+//     public Multiplicador(Somador somador) {
+//         this.somador = somador;
+//     }
+//     public Multiplicador set(Numero valor) {
+//         this.somador.set(valor);
+//         return this;
+//     }
+//     public Multiplicador vezes(Numero outro) {
+//         // verificar se somador tá zerado
+//         if (somador.resultado().valor() == 0) { return this; }
+//         // soma positiva e valor positivo -- abs!
+//         // soma positiva e valor negativo
+//         // soma positivo e valor positivo
+
+//         Numero aux = outro.duplica();
+//         while (aux.valor() > 0) {
+//             somador.
+//             aux.dec();
+//         }
+//         return this;
+//     }
+// }
