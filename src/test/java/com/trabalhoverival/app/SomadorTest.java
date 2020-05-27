27e8953139c859623a457ad779f7e5197ef72192
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-05-27 19:17:29 -0300

    criado classe de teste Somador

---
+++ src/test/java/com/trabalhoverival/app/SomadorTest.java
@@ -0,0 +1,11 @@
+package com.trabalhoverival.app;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SomadorTest {
+
+}
